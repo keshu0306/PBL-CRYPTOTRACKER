c796b967b1c10e6751164abd376fa539d275826d
Kunal Chousali <kunalchousali5@gmail.com>
2025-04-22 08:49:52 +0000

    add function to the swap button

--- src/components/swap-dialog.tsx
+++ src/components/swap-dialog.tsx
@@ -11,12 +11,14 @@ import {Button} from '@/components/ui/button';
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
 import {useEffect, useState} from "react";
 import {Input} from "@/components/ui/input";
+import {ArrowDown, ArrowUp} from "lucide-react";
 
 const SwapDialog = () => {
   const [isMounted, setIsMounted] = useState(false);
   const [payCoin, setPayCoin] = useState('');
   const [receiveCoin, setReceiveCoin] = useState('');
   const [payAmount, setPayAmount] = useState('');
+  const [receiveAmount, setReceiveAmount] = useState('0');
 
   useEffect(() => {
     setIsMounted(true);
@@ -24,14 +26,21 @@ const SwapDialog = () => {
 
   const handlePayCoinChange = (value: string) => {
     setPayCoin(value);
+    // In real app: Fetch the receiveCoin amount based on payCoin and amount
+    setReceiveAmount('0');
   };
 
   const handleReceiveCoinChange = (value: string) => {
     setReceiveCoin(value);
+    // In real app: Fetch the receiveCoin amount based on payCoin and amount
+    setReceiveAmount('0');
   };
 
   const handlePayAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPayAmount(e.target.value);
+    const amount = e.target.value;
+    setPayAmount(amount);
+    // In real app: Fetch the receiveCoin amount based on payCoin
+    setReceiveAmount('0');
   };
 
   return (
@@ -72,13 +81,9 @@ const SwapDialog = () => {
               </SelectContent>
             </Select>
           </div>
-
-          <div className="grid grid-cols-1 items-center gap-1 justify-center">
-            <img
-              src="https://picsum.photos/32/32"
-              alt="Switch Icon"
-              className="rounded-full"
-            />
+          <div className="grid grid-cols-4 items-center gap-1 justify-center">
+            <ArrowUp className="col-span-2 justify-self-end"/>
+            <ArrowDown className="col-span-2 justify-self-start"/>
           </div>
 
           <div className="grid grid-cols-12 items-center gap-4">
@@ -88,6 +93,7 @@ const SwapDialog = () => {
               placeholder="Amount"
               className="col-span-4"
               disabled
+              value={receiveAmount}
             />
             <Select onValueChange={handleReceiveCoinChange}>
               <SelectTrigger className="col-span-4">
