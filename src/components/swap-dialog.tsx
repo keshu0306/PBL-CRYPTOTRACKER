48064f0fdbc7d8eefa747d38314645086ccf9183
Kunal Chousali <kunalchousali5@gmail.com>
2025-04-29 08:43:47 +0000

    in the swap button there is an option to connect to wallet open this interface when a user taps on it

--- src/components/swap-dialog.tsx
+++ src/components/swap-dialog.tsx
@@ -1,19 +1,21 @@
 'use client';
 
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Dialog,
   DialogContent,
   DialogHeader,
   DialogTitle,
+  DialogTrigger,
 } from '@/components/ui/dialog';
 import {Button} from '@/components/ui/button';
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import {Input} from "@/components/ui/input";
 import {ArrowDown, ArrowUp} from "lucide-react";
 import {useToast} from "@/hooks/use-toast";
-import { currencies } from "@/lib/currencies";
+import {currencies} from "@/lib/currencies";
+import {X} from "lucide-react";
 
 const SwapDialog = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -23,6 +25,8 @@ const SwapDialog = () => {
   const [receiveAmount, setReceiveAmount] = useState('');
   const [isConnected, setIsConnected] = useState(false);
   const {toast} = useToast();
+  const [showConnectWallet, setShowConnectWallet] = useState(false); // State to control visibility of wallet connection options
+
 
   useEffect(() => {
     setIsMounted(true);
@@ -189,84 +193,118 @@ const SwapDialog = () => {
     });
   };
 
-  return (
-    <DialogContent className="sm:max-w-[425px] backdrop-blur-xl bg-secondary/90 border rounded-md"> {/* Adjusted transparency */}
-      <DialogHeader>
-        <DialogTitle>Swap</DialogTitle>
-      </DialogHeader>
-      <div className="grid gap-4 py-4">
-        <div className="grid grid-cols-4 items-center gap-4">
-          <div className="col-span-3 font-medium">Connect Your Wallet</div>
-          <Button variant="secondary" className="col-span-1 rounded-full" onClick={connectWallet} disabled={isConnected}>
-            {isConnected ? 'Connected' : 'Connect'}
-          </Button>
-        </div>
-
-        <div className="grid grid-cols-12 items-center gap-4">
-          <div className="col-span-4 font-medium">Pay</div>
-          <Input
-            type="number"
-            placeholder="Amount"
-            className="col-span-4 rounded-md transition-shadow duration-200 focus:shadow-outline" // Added transition
-            value={payAmount}
-            onChange={handlePayAmountChange}
-          />
-          <Select onValueChange={handlePayCoinChange}>
-            <SelectTrigger className="col-span-4 rounded-md">
-              <SelectValue placeholder="Select Coin"/>
-            </SelectTrigger>
-            <SelectContent className="rounded-md">
-              {isMounted ? (
-                <>
-                  {currencies.sort((a, b) => a.name.localeCompare(b.name)).map((currency) => (
-                    <SelectItem key={currency.code} value={currency.code} className="rounded-md">{currency.name} ({currency.symbol})</SelectItem>
-                  ))}
-                </>
-              ) : null}
-            </SelectContent>
-          </Select>
-        </div>
-        <div className="grid grid-cols-12 items-center gap-1 justify-center"> {/* Adjusted grid cols */}
-          {/* Added transition and group */}
-          <div className="col-start-6 col-span-2 flex justify-center group">
-            <ArrowUp className="h-4 w-4 transition-transform duration-200 group-hover:translate-y-[-2px]"/>
-            <ArrowDown className="h-4 w-4 transition-transform duration-200 group-hover:translate-y-[2px]"/>
-          </div>
-        </div>
-
-
-        <div className="grid grid-cols-12 items-center gap-4">
-          <div className="col-span-4 font-medium">Receive</div>
-          <Input
-            type="number"
-            placeholder="Amount"
-            className="col-span-4 rounded-md transition-shadow duration-200 focus:shadow-outline" // Added transition
-            disabled
-            value={receiveAmount}
-          />
-          <Select onValueChange={handleReceiveCoinChange}>
-            <SelectTrigger className="col-span-4 rounded-md">
-              <SelectValue placeholder="Select Coin"/>
-            </SelectTrigger>
-            <SelectContent className="rounded-md">
-              {isMounted ? (
-                <>
-                  {currencies.sort((a, b) => a.name.localeCompare(b.name)).map((currency) => (
-                    <SelectItem key={currency.code} value={currency.code} className="rounded-md">{currency.name} ({currency.symbol})</SelectItem>
-                  ))}
-                </>
-              ) : null}
-            </SelectContent>
-          </Select>
-        </div>
-        <Button variant="secondary" className="rounded-full" onClick={handleSwap} disabled={!payCoin || !receiveCoin || !payAmount}>
-          Swap
+  const renderWalletConnectOptions = () => (
+    <div className="py-4">
+      <DialogTitle>Select Portfolio to Connect</DialogTitle>
+      <p className="text-sm text-muted-foreground px-6">
+        Connect your wallets and exchanges to manage all your assets together now.
+      </p>
+      <div className="grid gap-4 py-4 px-6">
+        <Button variant="outline" className="rounded-full justify-start">
+          <img src="https://assets.coingecko.com/coins/images/1344/small/binance.png?1547034769" alt="Binance" className="w-6 h-6 mr-2 rounded-full"/>
+          MetaMask
         </Button>
-        <div className="grid grid-cols-2 items-center gap-4 bg-secondary rounded-lg p-3"> {/* Added padding */}
-          <div className="col-span-1 font-medium">Save on CoinStats Fees</div>
-          <Button variant="secondary" className="col-span-1 rounded-full">Go Premium</Button>
-        </div>
+        <Button variant="outline" className="rounded-full justify-start">
+          <img src="https://assets.coingecko.com/coins/images/1344/small/binance.png?1547034769" alt="Binance" className="w-6 h-6 mr-2 rounded-full"/>
+          Trust Wallet
+        </Button>
+        <Button variant="outline" className="rounded-full justify-start">
+          <img src="https://assets.coingecko.com/coins/images/1344/small/binance.png?1547034769" alt="Binance" className="w-6 h-6 mr-2 rounded-full"/>
+          Ethereum Wallet
+        </Button>
+        <Button variant="secondary" className="rounded-full justify-start">
+          ... Connect Other
+        </Button>
       </div>
+    </div>
+  );
+
+  return (
+    <DialogContent
+      className="sm:max-w-[900px] backdrop-blur-xl bg-secondary/90 border rounded-md"
+    >
+      {showConnectWallet ? (
+        renderWalletConnectOptions()
+      ) : (
+        <>
+          <DialogHeader>
+            <DialogTitle>Swap</DialogTitle>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <div className="col-span-3 font-medium">Connect Your Wallet</div>
+              <Button variant="secondary" className="col-span-1 rounded-full" onClick={() => setShowConnectWallet(true)} disabled={isConnected}>
+                {isConnected ? 'Connected' : 'Connect'}
+              </Button>
+            </div>
+
+            <div className="grid grid-cols-12 items-center gap-4">
+              <div className="col-span-4 font-medium">Pay</div>
+              <Input
+                type="number"
+                placeholder="Amount"
+                className="col-span-4 rounded-md transition-shadow duration-200 focus:shadow-outline" // Added transition
+                value={payAmount}
+                onChange={handlePayAmountChange}
+              />
+              <Select onValueChange={handlePayCoinChange}>
+                <SelectTrigger className="col-span-4 rounded-md">
+                  <SelectValue placeholder="Select Coin"/>
+                </SelectTrigger>
+                <SelectContent className="rounded-md">
+                  {isMounted ? (
+                    <>
+                      {currencies.sort((a, b) => a.name.localeCompare(b.name)).map((currency) => (
+                        <SelectItem key={currency.code} value={currency.code} className="rounded-md">{currency.name} ({currency.symbol})</SelectItem>
+                      ))}
+                    </>
+                  ) : null}
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="grid grid-cols-12 items-center gap-1 justify-center"> {/* Adjusted grid cols */}
+              {/* Added transition and group */}
+              <div className="col-start-6 col-span-2 flex justify-center group">
+                <ArrowUp className="h-4 w-4 transition-transform duration-200 group-hover:translate-y-[-2px]"/>
+                <ArrowDown className="h-4 w-4 transition-transform duration-200 group-hover:translate-y-[2px]"/>
+              </div>
+            </div>
+
+
+            <div className="grid grid-cols-12 items-center gap-4">
+              <div className="col-span-4 font-medium">Receive</div>
+              <Input
+                type="number"
+                placeholder="Amount"
+                className="col-span-4 rounded-md transition-shadow duration-200 focus:shadow-outline" // Added transition
+                disabled
+                value={receiveAmount}
+              />
+              <Select onValueChange={handleReceiveCoinChange}>
+                <SelectTrigger className="col-span-4 rounded-md">
+                  <SelectValue placeholder="Select Coin"/>
+                </SelectTrigger>
+                <SelectContent className="rounded-md">
+                  {isMounted ? (
+                    <>
+                      {currencies.sort((a, b) => a.name.localeCompare(b.name)).map((currency) => (
+                        <SelectItem key={currency.code} value={currency.code} className="rounded-md">{currency.name} ({currency.symbol})</SelectItem>
+                      ))}
+                    </>
+                  ) : null}
+                </SelectContent>
+              </Select>
+            </div>
+            <Button variant="secondary" className="rounded-full" onClick={handleSwap} disabled={!payCoin || !receiveCoin || !payAmount}>
+              Swap
+            </Button>
+            <div className="grid grid-cols-2 items-center gap-4 bg-secondary rounded-lg p-3"> {/* Added padding */}
+              <div className="col-span-1 font-medium">Save on CoinStats Fees</div>
+              <Button variant="secondary" className="col-span-1 rounded-full">Go Premium</Button>
+            </div>
+          </div>
+        </>
+      )}
     </DialogContent>
   );
 };
